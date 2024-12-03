215dda0c89d2a710c407b77fcaecb5234f5a8558
Fraz Khan <fraz9838@gmail.com>
2024-12-03 12:20:53 +0500

    setup convex-auth and middleware

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import {
+  convexAuthNextjsMiddleware,
+  createRouteMatcher,
+  isAuthenticatedNextjs,
+  nextjsMiddlewareRedirect,
+} from "@convex-dev/auth/nextjs/server";
+
+const isPublic = createRouteMatcher(["/signin"]);
+
+export default convexAuthNextjsMiddleware((request) => {
+  if (!isPublic(request) && !isAuthenticatedNextjs) {
+    return nextjsMiddlewareRedirect(request, "/signin");
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
