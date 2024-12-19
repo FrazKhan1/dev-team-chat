76c4b0d1cc391e9782e84ce088becf4df893923b
Fraz Khan <fraz9838@gmail.com>
2024-12-19 15:13:30 +0500

    create hook for add channels

---
+++ src/controllers/channels/api/use-create-channel.ts
@@ -0,0 +1,61 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useMutation } from "convex/react";
+import { useCallback, useMemo, useState } from "react";
+import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
+
+type RequestType = { name: string; workSpaceId: Id<"workspaces"> };
+type ResponseType = Id<"channels"> | null;
+
+type Options = {
+  onSuccess?: (data: ResponseType) => void;
+  onError?: (error: Error) => void;
+  onSettled?: () => void;
+  throwError?: boolean;
+};
+
+export const useCreateChannel = () => {
+  const [data, setData] = useState<ResponseType>(null);
+  const [error, setError] = useState<ResponseType>(null);
+  const [status, setStatus] = useState<
+    "success" | "error" | "settled" | "pending" | null
+  >(null);
+
+  const isPending = useMemo(() => status === "pending", [status]);
+  const isError = useMemo(() => status === "error", [status]);
+  const isSuccess = useMemo(() => status === "success", [status]);
+  const isSettled = useMemo(() => status === "settled", [status]);
+
+  const mutation = useMutation(api.channels.create);
+  const mutate = useCallback(
+    async (values: RequestType, options?: Options) => {
+      try {
+        setData(null);
+        setError(null);
+        setStatus("pending");
+        const response = await mutation(values);
+        options?.onSuccess?.(response);
+      } catch (error) {
+        setStatus("error");
+        if (options?.throwError) {
+          throw error;
+        }
+      } finally {
+        setStatus("settled");
+
+        options?.onSettled?.();
+      }
+    },
+    [mutation]
+  );
+
+  return {
+    mutate,
+    data,
+    error,
+    isPending,
+    isError,
+    isSuccess,
+    isSettled,
+  };
+};
--- src/controllers/channels/components/create-channel-modal.tsx
+++ src/controllers/channels/components/create-channel-modal.tsx
@@ -8,10 +8,18 @@ import { useCreateChannelModel } from "../store/use-create-channel-model";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { ChangeEvent, useState } from "react";
+import { useCreateChannel } from "../api/use-create-channel";
+import { useWorkSpaceId } from "@/hooks/use-workspace-id";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export const CreateChannelModal = () => {
   const [open, setOpen] = useCreateChannelModel();
   const [name, setName] = useState("");
+
+  const router = useRouter();
+  const workSpaceId = useWorkSpaceId();
+  const { mutate, isPending } = useCreateChannel();
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.replace(/\s+/g, "-").toLowerCase();
@@ -23,16 +31,30 @@ export const CreateChannelModal = () => {
     setName("");
   };
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    mutate(
+      { name, workSpaceId },
+      {
+        onSuccess: (id) => {
+          handleClose();
+          toast.success("Channel created successfully");
+          router.push(`/workspace/${workSpaceId}/channel/${id}`);
+        },
+      }
+    );
+  };
+
   return (
     <Dialog open={open} onOpenChange={handleClose}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Add a channel</DialogTitle>
         </DialogHeader>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleSubmit}>
           <Input
             value={name}
-            disabled={false}
+            disabled={isPending}
             onChange={handleChange}
             required
             autoFocus
@@ -40,10 +62,10 @@ export const CreateChannelModal = () => {
             maxLength={80}
             placeholder="e.g. plan-budget"
           />
+          <div className="flex justify-end">
+            <Button>Add</Button>
+          </div>
         </form>
-        <div className="flex justify-end">
-          <Button>Add</Button>
-        </div>
       </DialogContent>
     </Dialog>
   );
